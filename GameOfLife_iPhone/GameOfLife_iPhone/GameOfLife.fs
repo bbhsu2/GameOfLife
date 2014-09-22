9ccde6cbd3872eb212c75cacccf1fd2a3a48dc9f
Bernard Hsu <bernard.hsu@allgoodpeople.us>
2014-09-21 22:26:53 -0500

    new amount_neighbors function

--- GameOfLife_iPhone/GameOfLife_iPhone/GameOfLife.fs
+++ GameOfLife_iPhone/GameOfLife_iPhone/GameOfLife.fs
@@ -7,7 +7,7 @@ type Cell =
     | Alive
     | Dead
 
-let rnd = new Random()
+let rnd = new System.Random()
 
 let setBoard = 
     Array2D.init 10 10 (fun i j ->  
@@ -19,30 +19,43 @@ let flatten board =
     board |> Seq.cast<Cell> |> Seq.toArray
 
 let amount_neighbours (board: Cell[,]) (pos: int * int) =
-    let range (n: int) (limit: int) = 
-        let min = if n < 1 then 0 else n - 1
-        let max = if n > (limit - 2) then (limit - 1) else n + 1
-        [min .. max]
+    let alive board pos = 
+        let (x, y) = pos
+        if x < 0 || x >= Array2D.length1 board ||
+           y < 0 || y >= Array2D.length2 board then
+            false
+        else
+            board.[x, y] = Alive
 
-    let x_range = range (fst pos) (Array2D.length1 board)
-    let y_range = range (snd pos) (Array2D.length2 board)
+    let vicinity x = seq { x - 1 .. x + 1 }
 
-    List.sum [for x in x_range ->
-        List.sum [for y in y_range -> if board.[x, y] = Alive && (x, y) <> pos then 1 else 0]]
+    seq {
+        for x in vicinity (fst pos) do
+        for y in vicinity (snd pos) do
+        if (x, y) <> pos && alive board (x, y) then
+            yield true
+    } |> Seq.length
+//let amount_neighbours (board: Cell[,]) (pos: int * int) =
+//    let range (n: int) (limit: int) = 
+//        let min = if n < 1 then 0 else n - 1
+//        let max = if n > (limit - 2) then (limit - 1) else n + 1
+//        [min .. max]
+//
+//    let x_range = range (fst pos) (Array2D.length1 board)
+//    let y_range = range (snd pos) (Array2D.length2 board)
+//
+//    List.sum [for x in x_range ->
+//        List.sum [for y in y_range -> if board.[x, y] = Alive && (x, y) <> pos then 1 else 0]]
 
 let lifecycle (board: Cell[,]) = 
     Array2D.init (Array2D.length1 board) (Array2D.length2 board) (fun i j ->
         let neighbours = amount_neighbours board (i, j)
         match neighbours with
-            | 2 -> board.[i, j]
-            | 3 -> Alive
-            | _ -> Dead)
-
-let rec process_game (board: Cell[,]) (n: int) =
-    match n with
-        | x when x > 0 -> 
-            printfn "Iteration"
-            printfn "%A" board
-            process_game (lifecycle board) (n - 1)
-        | _ -> 0
-
+        | 2 -> board.[i,j]
+        | 3 -> Alive
+        | _ -> Dead)
+//        match (board.[i,j], neighbours) with
+//            | (_, 2) -> Alive//board.[i, j]
+//            | (Alive, 3) -> Alive
+//            | (Dead, 3) -> Alive
+//            | (_, _) -> Dead)
--- GameOfLife_iPhone/GameOfLife_iPhone/HomeViewController.fs
+++ GameOfLife_iPhone/GameOfLife_iPhone/HomeViewController.fs
@@ -20,36 +20,51 @@ type HomeViewController() as this=
 
     let workingBoard = ref setBoard
 
-    let mutable horizontalIndex = 0
-    let mutable verticalIndex = 0
     let mutable views = 
-        Array.init 100 (fun _ -> let mutable view = new UIView(this.View.Bounds)
-                                 view.Frame <- new RectangleF(new PointF(float32(viewWidth * verticalIndex), 
-                                                                         float32(viewHeight * horizontalIndex + 64)),
-                                                              new SizeF(float32(viewWidth), 
-                                                                        float32(viewHeight)))
-                                 horizontalIndex <- horizontalIndex + 1
-                                 if horizontalIndex = 9 then
-                                    horizontalIndex <- 0
-                                    verticalIndex <- verticalIndex + 1
-                                 view)
+        Array.init 100 (fun x -> new UIView(this.View.Bounds))
+    
+    let Frames = 
+        let rectangles = new List<RectangleF>()
+        for i = 0 to 9 do
+            for j = 0 to 9 do
+                rectangles.Add(new RectangleF(new PointF( float32(viewWidth * j), float32(viewHeight * i + 64)), new SizeF( float32(viewWidth), float32(viewHeight)))) 
+            done
+        done
+        rectangles
+//    let mutable views = 
+//        Array.init 100 (fun _ -> let mutable view = new UIView(this.View.Bounds)
+//                                 view.Frame <- new RectangleF(new PointF(float32(viewWidth * horizontalIndex), 
+//                                                                         float32(viewHeight * verticalIndex + 64)),
+//                                                              new SizeF(float32(viewWidth), 
+//                                                                        float32(viewHeight)))
+//                                 horizontalIndex <- horizontalIndex + 1
+//                                 if horizontalIndex = 9 then
+//                                    horizontalIndex <- 0
+//                                    verticalIndex <- verticalIndex + 1
+//                                 view)
 
     let refreshColors (fromBoard : Cell[,]) () = 
-        Array.iter2(fun (x:Cell) (y:UIView) -> match x with
-                                               |Alive -> y.BackgroundColor <- UIColor.Black
-                                               |Dead -> y.BackgroundColor <- UIColor.White) (flatten fromBoard) views
+        let flatBoard = flatten fromBoard
+        views |> Array.iteri(fun i x -> match flatBoard.[i] with
+                                        |Alive -> x.BackgroundColor <- UIColor.Black
+                                        |Dead -> x.BackgroundColor <- UIColor.White)
+//        Array.iter2(fun (x:Cell) (y:UIView) -> match x with
+//                                               |Alive -> y.BackgroundColor <- UIColor.Black
+//                                               |Dead -> y.BackgroundColor <- UIColor.White) (flatten fromBoard) views
 
     let StartAlgorithm = 
         EventHandler(fun _ _ -> this.NavigationItem.RightBarButtonItem.Enabled <- false
                                 this.InvokeOnMainThread( fun() -> async {
                                 while !continuationToken do
+                                    printfn "%A" !workingBoard
                                     workingBoard := (lifecycle !workingBoard)
-                                    UIView.Animate(float(1.0f),
+                                    printfn "%A" !workingBoard
+                                    UIView.Animate(float(0.6f),
                                                    float(0.0f),
                                                    UIViewAnimationOptions.TransitionNone,
                                                    new NSAction( refreshColors !workingBoard ),
                                                    fun() -> ())
-                                    do! Async.Sleep(1100)
+                                    do! Async.Sleep(700)
                                     fitnessLabel.Text <- "Fitness: " + (viewWidth).ToString()} |> Async.StartImmediate))
     
     do
@@ -59,38 +74,16 @@ type HomeViewController() as this=
         this.View.Frame <- UIScreen.MainScreen.Bounds
         this.View.BackgroundColor <- UIColor.White
 
-        refreshColors !workingBoard ()
+        let flatBoard = flatten !workingBoard
 
+        views
+        |> Array.iteri(fun i x -> match flatBoard.[i] with
+                                  |Alive -> x.BackgroundColor <- UIColor.Black
+                                  |Dead -> x.BackgroundColor <- UIColor.White
+                                  x.Frame <- Frames.[i])
+
+        printfn "%A" !workingBoard
+//
+//        refreshColors !workingBoard ()
+//
         this.View.AddSubviews views
-
-
-
-
-
-
-
-
-        //
-//        Array.iter2(fun (x:Cell) (y:UIView) -> match x with
-//                                               |Alive -> y.BackgroundColor <- UIColor.Black
-//                                               |Dead -> y.BackgroundColor <- UIColor.White) (flatten board) views
-
-
-        //    let updateUI = fun _ _ ->
-//
-//        let rec process_game (board: Cell[,]) (n: int) =
-//            if n > 0 then
-//                this.InvokeOnMainThread( fun () -> 
-//                Array.iter2(fun (x:Cell) (y:UIView) -> 
-//                                                       match x with
-//                                                       |Alive -> y.BackgroundColor <- UIColor.Black
-//                                                       |Dead -> y.BackgroundColor <- UIColor.White) (flatten board) views
-//                
-//                process_game (lifecycle board) (n - 1)
-//                )
-//            else ()
-//
-//        process_game board 10
-//
-//
-//    let iterate = EventHandler(updateUI)
